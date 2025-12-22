92a6054da4bbeecd18e33734ae9b550aae142c74
Cesar Rosales <cesar1rosales@gmail.com>
2025-12-22 08:49:22 -0800

    add canary logging to debug aws

--- app/services/band_service.py
+++ app/services/band_service.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional
 from sqlalchemy.orm import Session
 from app.repositories.band_repo import BandRepository
@@ -5,7 +6,10 @@ from app.repositories.band_repo import BandRepository
 class BandService:
     def __init__(self, repo: Optional[BandRepository] = None):
         self.repo = repo or BandRepository()
+        self.logger = logging.getLogger(__name__)
 
     def list_bands(self, db: Session):
+        self.logger.info("Canary: BandService.list_bands start")
         bands = self.repo.list_bands(db)
+        self.logger.info("Canary: BandService.list_bands result_count=%s", len(bands))
         return [{"id": b.id, "name": b.name} for b in bands]
